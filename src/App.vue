988ad677168c14b082e15e4a7d289aeedfc2ecc2
jmoore914 <jmoore914@gmail.com>
2023-04-21 13:03:30 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,21 @@
 <script setup lang="ts">
 
 import settingsIconSrc from './assets/settingsIcon.svg';
+import fs from 'fs';
+import path from 'path';
+
+const appData = process.env.APPDATA || (process.platform === 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share');
+console.log(appData);
+const settingsDirectory = path.join(appData, 'CrosswordManagerApp');
+console.log(settingsDirectory);
+if(!fs.existsSync(appData)){
+	fs.mkdirSync(settingsDirectory);
+}
+const settingsPath = path.join(settingsDirectory, 'settings.json');
+if(!fs.existsSync(settingsPath)){
+	const blankSettings = {installDirectory: null, nytCookie: null};
+	fs.writeFileSync(settingsPath, JSON.stringify(blankSettings, null, 2));
+}
 
 function openSettings(): void{
 	console.log('settings');
